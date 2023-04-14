9abd88f48a9c03cb52055988d404e946701f6d85
Maksym-Zhuravel <allwegotizus1@gmail.com>
2023-04-14 19:57:42 +0300

    Update js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,6 +2,8 @@ import throttle from "lodash.throttle";
 
 const STORAGE_KEY = 'feedback-form-state';
 
+let localData = {}
+
 
 
 const refs = {
@@ -15,16 +17,15 @@ refs.input.addEventListener('input', throttle(saveToLocalStore, 500));
 refs.textarea.addEventListener('input', throttle(saveToLocalStore, 500));
 restoreLocalData()
 
-function saveToLocalStore() {
-    const localData = {
-        email: refs.input.value,
-        message: refs.textarea.value,
-    }
+function saveToLocalStore(evt) {
+   localData[evt.target.name] = evt.target.value;
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(localData))
 }
 
 function onFormSubmit(evt) {
+    console.log(localData)
+    
     evt.preventDefault();
 
     evt.currentTarget.reset();
